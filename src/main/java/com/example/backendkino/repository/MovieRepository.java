84e70ec07cdd9a520fbf28ca819c97d1885c7e56
Aleksandergreg <alek3691@stud.kea.dk>
2024-10-01 12:40:01 +0200

    Made @Getmapping /movies in MovieRestController

--- src/main/java/com/example/backendkino/repository/MovieRepository.java
+++ src/main/java/com/example/backendkino/repository/MovieRepository.java
@@ -3,6 +3,9 @@ package com.example.backendkino.repository;
 import com.example.backendkino.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MovieRepository extends JpaRepository<Movie, Integer> {
+import java.util.List;
+
+public interface MovieRepository extends JpaRepository<Movie, String> {
+    List<Movie> findByTitle(String string);
 
 }
