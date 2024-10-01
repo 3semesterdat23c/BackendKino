447a207458d65dc22caef2e64bb358eb8d132437
AndersBondeLudvigsen <144429290+AndersBondeLudvigsen@users.noreply.github.com>
2024-10-01 13:02:42 +0200

    Made Add and Delete movie

--- src/main/java/com/example/backendkino/controller/MovieRestController.java
+++ src/main/java/com/example/backendkino/controller/MovieRestController.java
@@ -6,8 +6,10 @@ import com.example.backendkino.repository.MovieRepository;
 import com.example.backendkino.service.ApiServiceGetMovies;
 import org.aspectj.weaver.ast.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +28,28 @@ public class MovieRestController {
     public List<Movie> getAllMovies(){
         return movieRepository.findAll();
     }
+
+
+    @PostMapping("/movies")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Movie addMovie(@RequestBody Movie movie) {
+        return movieRepository.save(movie);
+    }
+
+    @DeleteMapping("/movie/{id}")
+    public ResponseEntity<String> deleteMovie(@PathVariable String id) {
+        System.out.println("Attempting to delete movie with ID: " + id);
+        if (!movieRepository.existsById(id)) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body("{\"message\": \"Movie not found\"}");
+        }
+        movieRepository.deleteById(id);
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body("{\"message\": \"Movie deleted successfully\"}");
+    }
+
+
 
 }
