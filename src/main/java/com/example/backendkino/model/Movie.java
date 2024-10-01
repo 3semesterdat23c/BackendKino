504aeac3a6f421886ca6217dc267353a590a8b4f
AndersBondeLudvigsen <144429290+AndersBondeLudvigsen@users.noreply.github.com>
2024-10-01 12:29:39 +0200

    Made Fetch for movies

--- src/main/java/com/example/backendkino/model/Movie.java
+++ src/main/java/com/example/backendkino/model/Movie.java
@@ -1,20 +1,129 @@
 package com.example.backendkino.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
-import java.util.ArrayList;
 
 @Entity
 public class Movie {
-
     @Id
-    private int movieId;
-
-    private String description;
-    private int length;
-    private double rating;
-    private int ticketPrice;
-    private int ageRestriction;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id; // Primary key
+    private String title;
+    private String year;
+    private String released;
+    private String runtime;
+    private String genre;
+    private String director;
+    private String actors;
     private String poster;
+    private String imdbRating;
+    private String imdbID;
+
+    public Movie(Integer id, String title, String year, String released, String runtime, String genre, String director, String actors, String poster, String imdbRating, String imdbID) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.released = released;
+        this.runtime = runtime;
+        this.genre = genre;
+        this.director = director;
+        this.actors = actors;
+        this.poster = poster;
+        this.imdbRating = imdbRating;
+        this.imdbID = imdbID;
+    }
+
+    // Default constructor
+    public Movie() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getReleased() {
+        return released;
+    }
+
+    public void setReleased(String released) {
+        this.released = released;
+    }
+
+    public String getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(String runtime) {
+        this.runtime = runtime;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getActors() {
+        return actors;
+    }
+
+    public void setActors(String actors) {
+        this.actors = actors;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public String getImdbRating() {
+        return imdbRating;
+    }
+
+    public void setImdbRating(String imdbRating) {
+        this.imdbRating = imdbRating;
+    }
+
+    public String getImdbID() {
+        return imdbID;
+    }
+
+    public void setImdbID(String imdbID) {
+        this.imdbID = imdbID;
+    }
 }
