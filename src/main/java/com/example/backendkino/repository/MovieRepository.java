670736564636dfb4c4809bcf646f49ebd1ca6e91
Karl Sixten Bjarnø <karlsixten@gmail.com>
2024-10-04 10:57:36 +0200

    Added existsByImdbId and findMovieByImdbId to MovieRepository.java

--- src/main/java/com/example/backendkino/repository/MovieRepository.java
+++ src/main/java/com/example/backendkino/repository/MovieRepository.java
@@ -4,8 +4,11 @@ import com.example.backendkino.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MovieRepository extends JpaRepository<Movie, String> {
     List<Movie> findByTitle(String string);
+    boolean existsByImdbID(String imdbId);
+    Optional<Movie> findByImdbID(String imdbId);
 
 }
