d41b56f791295055d164f9f258a70a5dd99a0ad9
Karl Sixten Bjarnø <karlsixten@gmail.com>
2024-10-08 14:35:14 +0200

    Added more init data which now: - Fetches movies from OMDB if none exists in our DB - Creates root admin if no admin user exists - Creates two theatres if none exits, also creates seats for said theatres - Creates two showings, one for each theatre with different Movies, different date and times and assigns the root user as admin for both showings

--- src/main/java/com/example/backendkino/config/InitDatabase.java
+++ src/main/java/com/example/backendkino/config/InitDatabase.java
@@ -2,12 +2,13 @@ package com.example.backendkino.config;
 
 import com.example.backendkino.model.Admin;
 import com.example.backendkino.model.Movie;
+import com.example.backendkino.model.Showing;
 import com.example.backendkino.repository.AdminRepository;
 import com.example.backendkino.repository.MovieRepository;
 import com.example.backendkino.repository.ShowingRepository;
 import com.example.backendkino.repository.TheatreRepository;
 import com.example.backendkino.service.ApiServiceGetMovies;
-import com.example.backendkino.service.ShowingService;
+import com.example.backendkino.service.ShowingServiceimpl;
 import com.example.backendkino.service.TheatreService;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,7 +36,7 @@ public class InitDatabase {
     private ShowingRepository showingRepository;
 
     @Autowired
-    private ShowingService showingService;
+    private ShowingServiceimpl showingServiceimpl;
 
     @Autowired
     private ApiServiceGetMovies apiServiceGetMovies;
@@ -72,19 +73,25 @@ public class InitDatabase {
 
         //Create two showings, one for each theatre if none exists
         if (showingRepository.count() == 0) {
-            showingService.createShowing(
+            Showing showing1 = new Showing(
+                    1,
+                    LocalDateTime.of(2024, 10, 31, 14, 00),
                     theatreRepository.getTheatresByTheatreId(1),
-                    movieRepository.findById("1").get(),
-                    LocalDateTime.of(2024, 10, 31, 12, 00),
-                    adminRepository.findByUsername("root").get()
+                    movieRepository.findMovieByMovieId(1),
+                    adminRepository.findByUsername("root").get(),
+                    LocalDateTime.of(2024, 10, 31, 12, 00)
                     );
+            showingServiceimpl.createShowing(showing1);
 
-            showingService.createShowing(
+            Showing showing2 = new Showing(
+                    2,
+                    LocalDateTime.of(2024, 10, 31, 18, 00),
                     theatreRepository.getTheatresByTheatreId(2),
-                    movieRepository.findById("2").get(),
-                    LocalDateTime.of(2024, 10, 30, 16, 30),
-                    adminRepository.findByUsername("root").get()
+                    movieRepository.findMovieByMovieId(2),
+                    adminRepository.findByUsername("root").get(),
+                    LocalDateTime.of(2024, 10, 30, 16, 30)
             );
+            showingServiceimpl.createShowing(showing2);
         } else {
             System.out.println("Database already has at least one showing.");
         }
