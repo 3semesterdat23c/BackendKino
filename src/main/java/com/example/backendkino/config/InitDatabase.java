286605c0f5345be134c4676e0f307acfd6d73429
Karl Sixten Bjarnø <karlsixten@gmail.com>
2024-10-08 13:39:51 +0200

    Added more init data which now: - Fetches movies from OMDB if none exists in our DB - Creates root admin if no admin user exists - Creates two theatres if none exits, also creates seats for said theatres - Creates two showings, one for each theatre with different Movies, different date and times and assigns the root user as admin for both showings

--- src/main/java/com/example/backendkino/config/InitDatabase.java
+++ src/main/java/com/example/backendkino/config/InitDatabase.java
@@ -4,11 +4,16 @@ import com.example.backendkino.model.Admin;
 import com.example.backendkino.model.Movie;
 import com.example.backendkino.repository.AdminRepository;
 import com.example.backendkino.repository.MovieRepository;
+import com.example.backendkino.repository.ShowingRepository;
+import com.example.backendkino.repository.TheatreRepository;
 import com.example.backendkino.service.ApiServiceGetMovies;
+import com.example.backendkino.service.ShowingService;
+import com.example.backendkino.service.TheatreService;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Configuration
@@ -21,16 +26,31 @@ public class InitDatabase {
     private AdminRepository adminRepository;
 
     @Autowired
+    private TheatreRepository theatreRepository;
+
+    @Autowired
+    private TheatreService theatreService;
+
+    @Autowired
+    private ShowingRepository showingRepository;
+
+    @Autowired
+    private ShowingService showingService;
+
+    @Autowired
     private ApiServiceGetMovies apiServiceGetMovies;
 
     @PostConstruct
     public void init() {
+        //Fetch movies from OMDB if none exists in our database
         if (movieRepository.count() == 0) {
             List<Movie> movies = apiServiceGetMovies.getMovies();
             movieRepository.saveAll(movies);
         } else {
             System.out.println("Database already populated with movies.");
         }
+
+        //Create root user if no admin exists
         if (adminRepository.count() == 0) {
             Admin admin = new Admin();
             admin.setUsername("root");
@@ -38,7 +58,35 @@ public class InitDatabase {
             admin.setPassword("root");
             adminRepository.save(admin);
         } else {
-            System.out.println("Root user already exists.");
+            System.out.println("Database already has at least one Admin user.");
+        }
+
+        //Create two theatres if none exists
+        //Also creates seats
+        if (theatreRepository.count() == 0) {
+            theatreService.createTheatre(20, 12);
+            theatreService.createTheatre(25, 16);
+        } else {
+            System.out.println("Database already has at least one theatre.");
+        }
+
+        //Create two showings, one for each theatre if none exists
+        if (showingRepository.count() == 0) {
+            showingService.createShowing(
+                    theatreRepository.getTheatresByTheatreId(1),
+                    movieRepository.findById("1").get(),
+                    LocalDateTime.of(2024, 10, 31, 12, 00),
+                    adminRepository.findByUsername("root").get()
+                    );
+
+            showingService.createShowing(
+                    theatreRepository.getTheatresByTheatreId(2),
+                    movieRepository.findById("2").get(),
+                    LocalDateTime.of(2024, 10, 30, 16, 30),
+                    adminRepository.findByUsername("root").get()
+            );
+        } else {
+            System.out.println("Database already has at least one showing.");
         }
     }
 }
